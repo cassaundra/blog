8df66279aa45e0235171caaf1c2a285c6bc780c8
Cassaundra <cass@cassaundra.io>
2020-04-23 20:59:05 -0700

    Improve spacebar demo

--- content/space.md
+++ content/space.md
@@ -10,5 +10,31 @@ categories = []
 +++
 
 ## Demo
-<input id="source">
-<script src="js/spacebar.js"></script>
+
+<form autocomplete="off">
+  <input id="demo-simple">
+  <select id="case" name="case">
+    <option value="snake">snake_case</option>
+    <option value="kebab">kebab-case</option>
+    <option value="camel">camelCase</option>
+    <option value="class">ClassCase</option>
+    <option value="train">Train-Case</option>
+  </select>
+</form>
+
+Type `let file reader = file lib::File reader::from file();`.
+
+<textarea id="demo-contextual"></textarea>
+<div id="editor">function foo(items) {
+    var x = "All this is syntax highlighted";
+    return x;
+}</div>
+    
+<script src="/ace-builds/src-noconflict/ace.js" type="text/javascript" charset="utf-8"></script>
+<script>
+    var editor = ace.edit("editor");
+    editor.setTheme("ace/theme/monokai");
+    editor.session.setMode("ace/mode/javascript");
+</script>
+
+<script src="/js/spacebar.js"></script>
--- static/js/spacebar.js
+++ static/js/spacebar.js
@@ -1,6 +1,6 @@
 // https://stackoverflow.com/questions/15975922/how-to-use-jquery-insertatcaret-function
 jQuery.fn.extend({
-    insertAtCaret: function(myValue){
+    insertAtCaret: function(myValue) {
         return this.each(function(i) {
             if (document.selection) {
                 // For browsers like Internet Explorer
@@ -8,13 +8,12 @@ jQuery.fn.extend({
                 sel = document.selection.createRange();
                 sel.text = myValue;
                 this.focus();
-            }
-            else if (this.selectionStart || this.selectionStart == '0') {
+            } else if (this.selectionStart || this.selectionStart == '0') {
                 // For browsers like Firefox and Webkit based
                 var startPos = this.selectionStart;
                 var endPos = this.selectionEnd;
                 var scrollTop = this.scrollTop;
-                this.value = this.value.substring(0, startPos)+myValue+this.value.substring(endPos,this.value.length);
+                this.value = this.value.substring(0, startPos) + myValue + this.value.substring(endPos, this.value.length);
                 this.focus();
                 this.selectionStart = startPos + myValue.length;
                 this.selectionEnd = startPos + myValue.length;
@@ -27,19 +26,79 @@ jQuery.fn.extend({
     }
 });
 
+// disclaimer: I am not a JavaScript developer! sorry for whatever the hell this is (:
 $(document).ready(function() {
-    var space = false;
-    $("#source").on("keypress", function(event) {
+    var spaceMode = false;
+    var lastSelectionStart = null;
+    $("#demo-simple").on("keypress", function(event) {
+        if (lastSelectionStart != this.selectionStart) {
+            spaceMode = false;
+        }
+
         if (event.which == 32) {
-            space = !space;
-            if (space) {
+            spaceMode = !spaceMode;
+            if (spaceMode) {
                 event.preventDefault();
             }
         } else {
-            if (space) {
-                $(this).insertAtCaret("_");
+            var mode = $("select[name=case]").val();
+
+            if (spaceMode) {
+                switch (mode) {
+                    case "snake": // snake_case
+                        $(this).insertAtCaret("_");
+                        break;
+                    case "kebab": // kebab-case
+                        $(this).insertAtCaret("-");
+                        break;
+                    case "camel": // camelCase
+                        event.preventDefault();
+                        $(this).insertAtCaret(String.fromCharCode(event.which).toUpperCase());
+                        break;
+                    case "class": // ClassCase
+                        event.preventDefault();
+                        $(this).insertAtCaret(String.fromCharCode(event.which).toUpperCase());
+                        break;
+                    case "train": // Train-Case
+                        $(this).insertAtCaret("-");
+                        event.preventDefault();
+                        $(this).insertAtCaret(String.fromCharCode(event.which).toUpperCase());
+                        break;
+                    default:
+                }
+            } else if (mode == "class" || mode == "train") {
+                // TODO
             }
-            space = false;
+            spaceMode = false;
         }
+        lastSelectionStart = this.selectionStart;
+    });
+
+    const rustKeywords = ["as", "async", "await", "break", "const", "continue", "crate", "dyn", "else", "enum", "extern", "false", "fn", "for", "if", "impl", "in", "let", "loop", "match", "mod", "move", "mut", "pub", "ref", "return", "self", "Self", "static", "struct", "super", "trait", "true", "type", "unsafe", "use", "where", "while"];
+    $("#demo-contextual").on("keypress", function(event) {
+        if (lastSelectionStart != this.selectionStart) {
+            spaceMode = false;
+        }
+
+        var char = String.fromCharCode(event.which);
+        var leading = this.value.substring(0, this.selectionStart);
+        var regexp = /[a-z0-9]+$/i;
+        var word = regexp.exec(leading)[0];
+
+        if (event.which == 32 && word != null && !rustKeywords.includes(word)) {
+            spaceMode = !spaceMode;
+            if (spaceMode) {
+                event.preventDefault();
+            }
+        } else if (spaceMode) {
+            if (regexp.test(char)) {
+                $(this).insertAtCaret("_");
+            } else {
+                $(this).insertAtCaret(" ");
+            }
+            spaceMode = false;
+        }
+
+        lastSelectionStart = this.selectionStart;
     });
 });
